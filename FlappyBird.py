3911111fee73cf34114f2e72a374b8eea425a728
EliasJuk <eliasp.juk@hotmail.com>
2022-01-29 08:54:25 -0300

    classe passaro

--- FlappyBird.py
+++ FlappyBird.py
@@ -9,7 +9,30 @@ IMAGEM_CANO = pygame.transform.scale2x(pygame.image.load(os.path.join('imgs', 'p
 IMAGEM_CHAO = pygame.transform.scale2x(pygame.image.load(os.path.join('imgs', 'base.png')))
 IMAGEM_BACKGROUND = pygame.transform.scale2x(pygame.image.load(os.path.join('imgs', 'bg.png')))
 IMAGENS_PASSARO = [
-    pygame.transform.scale2x(pygame.image.load(os.path.join('imgs', 'bird1.png'))),
-    pygame.transform.scale2x(pygame.image.load(os.path.join('imgs', 'bird2.png'))),
-    pygame.transform.scale2x(pygame.image.load(os.path.join('imgs', 'bird3.png'))),
+  pygame.transform.scale2x(pygame.image.load(os.path.join('imgs', 'bird1.png'))),
+  pygame.transform.scale2x(pygame.image.load(os.path.join('imgs', 'bird2.png'))),
+  pygame.transform.scale2x(pygame.image.load(os.path.join('imgs', 'bird3.png'))),
 ]
+
+pygame.font.init()
+FONTE_PONTOS = pygame.font.SysFont('arial', 50)
+
+
+class Passaro:
+  IMGS = IMAGENS_PASSARO
+
+  # animações da rotação
+  ROTACAO_MAXIMA = 25
+  VELOCIDADE_ROTACAO = 20
+  TEMPO_ANIMACAO = 5
+
+  # atributos do passaro
+  def __init__(self, x, y):
+    self.x = x
+    self.y = y
+    self.angulo = 0
+    self.velocidade = 0
+    self.altura = self.y
+    self.tempo = 0
+    self.contagem_imagem = 0
+    self.imagem = IMGS[0]
